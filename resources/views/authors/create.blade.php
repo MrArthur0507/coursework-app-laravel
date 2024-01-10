5c7da47175781de1be426600d64b5debd3ce0fb1
BuildTools <unconfigured@null.spigotmc.org>
2024-01-10 16:56:50 +0200

    Added view for author adding

--- resources/views/authors/create.blade.php
+++ resources/views/authors/create.blade.php
@@ -1,18 +1,25 @@
 <x-app-layout>
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container">
+        <h2>Add Author</h2>
+
+        <form method="POST" action="{{ route('authors.store') }}">
+            @csrf
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Author Name">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+            </div>
+
+            <div class="form-group">
+                <label for="bio">Bio</label>
+                <textarea class="form-control" id="bio" name="bio" rows="3" placeholder="Author Bio"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Add Author</button>
+        </form>
+    </div>
 </x-app-layout>
