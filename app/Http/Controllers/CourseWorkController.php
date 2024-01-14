d7bc64f5a3a73bac67f6b28ba7e925a8ace7f11b
BuildTools <unconfigured@null.spigotmc.org>
2024-01-14 21:33:23 +0200

    Added blank views for courseworks and improved logic

--- app/Http/Controllers/CourseWorkController.php
+++ app/Http/Controllers/CourseWorkController.php
@@ -2,43 +2,80 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use App\Models\CourseWork;
+use App\Models\Manager;
 use Illuminate\Http\Request;
 
 class CourseWorkController extends Controller
 {
     public function index()
     {
-        $courses = CourseWork::all();
-        return view('courses.index', compact('courses'));
+        $courseworks = CourseWork::all();
+        return view('courseworks.index', compact('courseworks'));
     }
 
-    public function show(CourseWork $course)
+    public function show(CourseWork $coursework)
     {
-        return view('courses.show', compact('course'));
+        return view('courseworks.show', compact('coursework'));
     }
 
     public function create()
     {
-        return view('courses.create');
+        $authors = Author::all();
+        $managers = Manager::all();
+        return view('courseworks.create', compact('authors', 'managers'));
     }
 
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'author_id' => 'required|exists:authors,id',
+            'manager_id' => 'required|exists:managers,id',
+        ]);
 
-        $course = CourseWork::create([
+        $coursework = CourseWork::create([
             'title' => $request->input('title'),
             'description' => $request->input('description'),
             'author_id' => $request->input('author_id'),
             'manager_id' => $request->input('manager_id'),
         ]);
 
-        return view('courses.index');
+        return redirect()->route('courseworks.index');
     }
 
-    public function destroy(CourseWork $course)
+    public function edit(CourseWork $coursework)
     {
-        $course->delete();
-        return view('courses.index');
+        $authors = Author::all();
+        $managers = Manager::all();
+        return view('courseworks.edit', compact('coursework', 'authors', 'managers'));
+    }
+
+    public function update(Request $request, CourseWork $coursework)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'author_id' => 'required|exists:authors,id',
+            'manager_id' => 'required|exists:managers,id',
+        ]);
+
+        $coursework->update([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'author_id' => $request->input('author_id'),
+            'manager_id' => $request->input('manager_id'),
+        ]);
+
+        return redirect()->route('courseworks.show', $coursework->id);
+    }
+
+    public function destroy(CourseWork $coursework)
+    {
+        $coursework->delete();
+
+        return redirect()->route('courseworks.index');
     }
 }
